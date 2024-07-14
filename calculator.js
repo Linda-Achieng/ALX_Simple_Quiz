f917b4ad27258f7b0084adf682871d18d2de453b
Linda Achieng <achienglinda555@gmail.com>
2024-07-14 16:20:12 +0300

    modified calculator.js

--- calculator.js
+++ calculator.js
@@ -1,25 +1,21 @@
-// Arithmetic functions
-function add(number1, number2) {
+function add(number1, number2 ){
   return number1 + number2;
 }
-
-function subtract(number1, number2) {
+function subtract(number1, number2){
   return number1 - number2;
 }
-
-function multiply(number1, number2) {
+function multiply(number1,number2){
   return number1 * number2;
 }
-
-function divide(number1, number2) {
-  if (number2 === 0) {
-      return "Error: Division by zero";
+function divide(number1,number2){
+  if(number2===0){
+    return  "Error: Division by zero";
   }
-  return number1 / number2;
+  return number1/number2
 }
 
 // Function to update the result
-function updateResult(result) {
+function updateResult(result){
   document.getElementById('calculation-result').textContent = result;
 }
 
@@ -31,12 +27,14 @@ document.getElementById('add').addEventListener('click', function() {
   updateResult(result);
 });
 
+
 document.getElementById('subtract').addEventListener('click', function() {
   const number1 = parseFloat(document.getElementById('number1').value) || 0;
   const number2 = parseFloat(document.getElementById('number2').value) || 0;
   const result = subtract(number1, number2);
   updateResult(result);
 });
+
 
 document.getElementById('multiply').addEventListener('click', function() {
   const number1 = parseFloat(document.getElementById('number1').value) || 0;
